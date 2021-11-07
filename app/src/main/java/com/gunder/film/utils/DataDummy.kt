f786808913c17e5244ba4b05e216fc9caecc3943
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-07 20:17:07 +0700

    membuat instuemented test [final]

--- app/src/main/java/com/gunder/film/utils/DataDummy.kt
+++ app/src/main/java/com/gunder/film/utils/DataDummy.kt
@@ -41,12 +41,12 @@ object DataDummy {
         tvShow.add(
             ListEntity(
                 88396,
-                null,
+                "Squid Game",
                 "The Falcon and the Winter Soldier",
                 "/6kbAMLteGO8yyewYau6bJ683sw7.jpg",
                 "/b0WmHGc8LHTdGCVzxRb3IBMur57.jpg",
-                "Following the events of “Avengers: Endgame”, the Falcon, Sam Wilson and the Winter Soldier, Bucky Barnes team up in a global adventure that tests their abilities, and their patience.",
-                "2018"
+                "Hundreds of cash-strapped players accept a strange invitation to compete in children's games—with high stakes. But, a tempting prize awaits the victor.",
+                null
             )
         )
         tvShow.add(
@@ -57,7 +57,7 @@ object DataDummy {
                 "/mrVoyDFiDSqfH4mkoRtccOv2vwh.jpg",
                 "/8z9qQkx7wA6FXpLV8Tiw9mfsRFK.jpg",
                 "In a world cleaved in two by a massive barrier of perpetual darkness, a young soldier uncovers a power that might finally unite her country. But as she struggles to hone her power, dangerous forces plot against her. Thugs, thieves, assassins and saints are at war now, and it will take more than magic to survive.",
-                "2018"
+                null
             )
         )
 
@@ -130,9 +130,10 @@ object DataDummy {
                 460465,
                 "/9yBVqNruk6Ykrwc32qrK2TIE5xw.jpg",
                 "Mortal Kombat",
-                null,
+                "Dune",
                 "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.",
-                "2018"
+                "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence-a commodity capable of unlocking humanity's greatest potential-only those who can conquer their fear will survive.",
+                "2021-09-15"
             )
         )
         movies.add(
@@ -142,7 +143,8 @@ object DataDummy {
                 "Godzilla vs. Kong",
                 null,
                 "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
-                "2018"
+                "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence-a commodity capable of unlocking humanity's greatest potential-only those who can conquer their fear will survive.",
+                "2021-09-15"
             )
         )
 
