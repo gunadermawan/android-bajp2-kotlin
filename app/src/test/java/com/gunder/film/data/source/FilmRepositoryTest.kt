af7c37b5838be2706dae9bdc9ba51251464e63ad
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-07 11:02:04 +0700

    memulai testing unit

---
+++ app/src/test/java/com/gunder/film/data/source/FilmRepositoryTest.kt
@@ -0,0 +1,24 @@
+package com.gunder.film.data.source
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class FilmRepositoryTest {
+
+    @Test
+    fun getAllMovies() {
+    }
+
+    @Test
+    fun getAllTvShow() {
+    }
+
+    @Test
+    fun getDetailMovies() {
+    }
+
+    @Test
+    fun getDetailTvShow() {
+    }
+}
