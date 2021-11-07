6f135d4bf1cb4add1be394eb62cdfc2e1cb54e4d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-07 15:52:54 +0700

    membuat fakerepository

--- app/src/test/java/com/gunder/film/data/source/FilmRepositoryTest.kt
+++ app/src/test/java/com/gunder/film/data/source/FilmRepositoryTest.kt
@@ -1,10 +1,16 @@
 package com.gunder.film.data.source
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.gunder.film.data.source.remote.RemoteDataSource
+import org.junit.Rule
+import org.mockito.Mockito.*
 
 import org.junit.Test
 
 class FilmRepositoryTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val remote = mock(RemoteDataSource::class.java)
 
     @Test
     fun getAllMovies() {
